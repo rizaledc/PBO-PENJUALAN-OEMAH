6ea27d7bac565aa0dd58137d24fcb9cce62269ae
yokaroo <yokap30@gmail.com>
2025-06-11 01:12:31 +0700

    Update Profile

--- src/main/java/com/penjualanrumah/model/User.java
+++ src/main/java/com/penjualanrumah/model/User.java
@@ -1,4 +1,3 @@
-
 package com.penjualanrumah.model;
 
 import jakarta.persistence.*;
@@ -8,7 +7,7 @@ import lombok.Data;
 @Entity
 @Table(name = "users")
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -38,5 +37,69 @@ public class User {
     public enum UserRole {
         BUYER,
         SELLER
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 }
